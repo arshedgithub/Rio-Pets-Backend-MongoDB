68141d2fc2bdac32a94ff597fab4609686708c11
arshedgithub <arshedahmedmail@gmail.com>
2025-04-25 10:14:59 +0530

    vercel configuration

--- src/app.ts
+++ src/app.ts
@@ -30,9 +30,11 @@ app.use('/api/categories', categoryRoutes);
 
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+if (process.env.NODE_ENV == "development") {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
 
 export default app;
